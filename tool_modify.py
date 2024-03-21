1b75e86d3170159d6675ca6423ccdbee7cea6bf2
congneb <51370224+congneb@users.noreply.github.com>
2024-03-21 14:21:39 +0700

    Update tool_modify.py

--- tool_modify.py
+++ tool_modify.py
@@ -4,7 +4,8 @@ raw_dir_file = sys.argv[1]
 with open(raw_dir_file + "/test_build.py", "r") as file:
 	filedata = file.read()
 
-filedata = filedata.replace("arduino-cli", "arduino_cli")
+arduino_cli_path = sys.argv[2]
+filedata = filedata.replace("arduino-cli", arduino_cli_path)
 
 with open(raw_dir_file + "/test_build_modified.py", "w") as file:
 	file.write(filedata)
