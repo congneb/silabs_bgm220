7998668d284481f14e6ca54dd2ca7c87d83f8ae0
congneb <51370224+congneb@users.noreply.github.com>
2024-03-22 09:55:05 +0700

    Update tool_modify_v2.py

--- tool_modify_v2.py
+++ tool_modify_v2.py
@@ -9,7 +9,7 @@ arduino_cli_path = sys.argv[2]
 filedata = filedata.replace("arduino-cli", arduino_cli_path)
 
 config_file_path = sys.argv[3]
-replace_str = "compile\", \"--config-file {0}".format(config_file_path)
+replace_str = "compile\", \"--config-file\", \"{0}".format(config_file_path)
 filedata = filedata.replace("compile", replace_str)
 
 with open(raw_dir_file + "/test_build_modified.py", "w") as file:
